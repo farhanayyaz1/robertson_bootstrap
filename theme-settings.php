c07037bf8b7b3963268834b6f345d0afd8ad87e4
James Robertson <jwrober@gmail.com>
2017-05-24 17:43:07 -0500

    Try 4 - New development setting works now.

--- theme-settings.php
+++ theme-settings.php
@@ -14,42 +14,41 @@
 * Implements hook_form_FORM_ID_alter().
 */
 function robertson_bootstrap_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
-  // Create vertical tabs for all Bootstrap related settings.
-  $form['robertson_customizations'] = array(
-    '#type' => 'vertical_tabs',
+
+  // Work-around for a core bug affecting admin themes.
+  // @see https://drupal.org/node/943212
+  if (isset($form_id)) {
+    return;
+  }
+
+  // Create vertical tabs for all Robertson Bootstrap related settings.
+  $form['robertson_bootstrap'] = array(
+    '#type'     => 'vertical_tabs',
     '#attached' => array(
-      'js'  => array(drupal_get_path('theme', 'bootstrap') . '/js/bootstrap.admin.js'),
+      'js'      => array(drupal_get_path('theme', 'bootstrap') . '/js/bootstrap.admin.js'),
     ),
-    '#prefix' => '<h2><small>' . t('Robertson Bootstrap Custom Settings') . '</small></h2>',
-    '#weight' => -10,
+    '#prefix'   => '<h2><small>' . t('Robertson Bootstrap Settings') . '</small></h2>',
+    '#weight'   => -20,
   );
+
   // Add our own vertical tab to base bootstrap settings
-  // ThemeDev
   $form['themedev'] = array(
-    '#type' => 'fieldset',
+    '#type'  => 'fieldset',
     '#title' => t('Development'),
-    '#group' => 'robertson_customizations',
+    '#group' => 'robertson_bootstrap',
   );
-  //Container
-  $form['themedev']['container'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Container'),
+
+  $form['themedev']['themedev_1'] = array(
+    '#type'        => 'fieldset',
+    '#title'       => t('Theme Registry Development'),
     '#collapsible' => TRUE,
-    '#collapsed' => TRUE,
+    '#collapsed'   => TRUE,
   );
    // Development Mode Settings (see settings[rebuild_registry] in .info)
-  $form['themedev']['container']['rebuild_registry'] = array(
+  $form['themedev']['themedev_1']['rebuild_registry'] = array(
     '#type'          => 'checkbox',
     '#title'         => t('Rebuild theme registry on every page.'),
     '#default_value' => theme_get_setting('rebuild_registry'),
     '#description'   => t('During theme development, it can be very useful to continuously <a href="https://drupal.org/node/173880#theme-registry">rebuild the theme registry</a>. <br /> <div class="alert alert-warning messages warning"><b>WARNING</b>: this is a huge performance penalty and must be turned off on production websites.</div>'),
   );
-/**  $form['robertson']['themedev']['siteid'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Site ID body class.'),
-   	'#description' => t('In order to have different styles of Robertson Bootstrap theme in a multisite/multi-theme environment you may find usefull to choose an "ID" and customize the look of each site/theme in custom-style.css file.'),
-    '#default_value' => theme_get_setting('siteid'),
-    '#size' => 10,
-	);
-*/
 }
